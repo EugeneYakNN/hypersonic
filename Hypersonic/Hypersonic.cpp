a6ab15a728c4fb2e1f0df2d90d494eee7b941306
Eugene Yakovlev <EugeneYakNN@gmail.com>
2019-04-24 13:07:08 +0300

    Merge latest changes.

--- Hypersonic/Hypersonic.cpp
+++ Hypersonic/Hypersonic.cpp
@@ -13,14 +13,12 @@ using namespace std;
 const int ENTITY_TYPE_PLAYER = 0;
 const int ENTITY_TYPE_BOMB = 1;
 
-
 template<class T>
 auto operator>>(std::istream & in, T& t) -> decltype(t.Read(in), in)
 {
     t.Read(in);
     return in;
 }
-
 
 template<class T>
 auto operator<<(std::ostream& os, const T& t) -> decltype(t.Print(os), os)
@@ -36,14 +34,11 @@ struct Rules
     int height;
     int myId;
 
-
-
     void Read(std::istream & in)
     {
         in >> width >> height >> myId; in.ignore();
     }
 };
-
 
 struct Entity
 {
@@ -88,14 +83,14 @@ public:
     }
     bool IsBox(int x, int y) const
     { 
-        return '0' == m_grid[y][x];
+        return '.' != m_grid[y][x] && 'X' != m_grid[y][x];
     }
     bool IsBoxSafeCheck(int x, int y) const
     { 
-        if (x < 0 || x >= (int)m_width || y < 0 || y >= (int)m_height)
+        if (x < 0 || x >= m_width || y < 0 || y >= m_height)
             return false;
         else
-            return '0' == m_grid[y][x];
+            return IsBox(x,y);
     }
     
     bool IsBoxAround(int x, int y) const
@@ -110,10 +105,36 @@ public:
 protected:
 };
 
+class Entities
+{
+public:
+    Entities(int myId, size_t size=0)
+        : m_myId(myId)
+        , m_entitiesList(size)
+        , m_myEntity(-1)
+    {}
+    void AddEntity(const Entity &e)
+    {
+        if (ENTITY_TYPE_PLAYER == e.entityType && m_myId == e.owner)
+        {
+            m_myEntity = m_entitiesList.size();
+        }
+        m_entitiesList.push_back(e);
+    }
+    const Entity & Me() const
+    {
+        return m_entitiesList[m_myEntity];
+    }
+//protected:
+    int m_myId;
+    int m_myEntity;
+    std::vector<Entity> m_entitiesList;
+};
+
 class GridCostEstimator
 {
 public:
-    GridCostEstimator(const Grid &grid)
+    GridCostEstimator(const Grid &grid, const Entities &entitiesList)
         : m_grid(grid)
         , m_width(m_grid.m_width)
         , m_height(m_grid.m_height)
@@ -121,11 +142,14 @@ public:
         , m_gridCost(m_height, m_zeroRow)
         , max(0)
     {
-        for (size_t y = 0; y < m_height; y++) {
-            for (size_t x = 0; x < m_width; x++) {
+        size_t bombsLeft = entitiesList.Me().param1;
+        size_t range = entitiesList.Me().param2;
+        for (int y = 0; y < m_height; y++) {
+            for (int x = 0; x < m_width; x++) {
                 if (m_grid.IsBox(x,y))
                 {
                     AddBox(x,y,3);
+                    
                 }
             }
         }        
@@ -138,35 +162,39 @@ public:
     
     void AddBox(size_t boxX, size_t boxY, size_t bombRange)
     {
-        int rangeX[2] = {static_cast<int>(boxX)- static_cast<int>(bombRange)+1, static_cast<int>(boxX)+ static_cast<int>(bombRange)-1};
+        int rangeX[2] = {boxX-bombRange+1, boxX+bombRange-1};
         clip<int>(rangeX[0], 0, m_width);
         clip<int>(rangeX[1], 0, m_width);
         
-        int rangeY[2] = {(int)(boxY)-bombRange+1, (int)(boxY)+bombRange-1};
+        int rangeY[2] = {boxY-bombRange+1, boxY+bombRange-1};
         clip<int>(rangeY[0], 0, m_height);
         clip<int>(rangeY[1], 0, m_height);
 
         for (int x = rangeX[0]; x < rangeX[1]; x++) {
-            if (boxX != x && !m_grid.IsBox(x,boxY))
+            int y = boxY;
+            int &cost = m_gridCost[y][x];
+            if (cost >= 0 && boxX != x && !m_grid.IsBox(x,y))
             {
-                m_gridCost[boxY][x]++;
-                if (m_gridCost[boxY][x] > max)
+                cost += 2;
+                if (cost > max)
                 {
-                    max = m_gridCost[boxY][x];
+                    max = cost;
                     maxCell.x = x;
-                    maxCell.y = boxY;
+                    maxCell.y = y;
                 }
             }
         }
 
         for (int y = rangeY[0]; y < rangeY[1]; y++) {
-            if (boxY != y && !m_grid.IsBox(boxX,y))
+            int x = boxX;
+            int &cost = m_gridCost[y][x];
+            if (cost >=0 && boxY != y && !m_grid.IsBox(x,y))
             {
-                m_gridCost[y][boxX]++;
-                if (m_gridCost[y][boxX] > max)
+                cost += 2;
+                if (cost > max)
                 {
-                    max = m_gridCost[y][boxX];
-                    maxCell.x = boxX;
+                    max = cost;
+                    maxCell.x = x;
                     maxCell.y = y;
                 }
             }
@@ -192,6 +220,7 @@ protected:
     typedef std::vector<CellRowCost> GridCost;
     const CellRowCost m_zeroRow;
     GridCost m_gridCost;
+    std::vector<Entity> m_entitiesList;
     
     int max;
 
@@ -199,42 +228,35 @@ protected:
  
 int main()
 {
-    //int width;
-    //int height;
-    //int myId;
-    //cin >> width >> height >> myId; cin.ignore();
-    Rules rules;
-    cin >> rules;
+    int width;
+    int height;
+    int myId;
+    cin >> width >> height >> myId; cin.ignore();
     
 
     // game loop
     while (1) {
         
-        Grid grid(rules.width, rules.height);
-        for (int i = 0; i < rules.height; i++) {
+        Grid grid(width, height);
+        for (int i = 0; i < height; i++) {
             string row;
             cin >> row; cin.ignore();
             cerr << row << endl;
             grid.ReadRow(row);
         }
         
-        for (int x = 0; x < rules.width; x++)
+        for (int x = 0; x < width; x++)
         {
             cerr << "--";
         };
         cerr << endl;
         
-        GridCostEstimator gridCost(grid);
-        cerr << gridCost;
-        
+    
         int entities;
         cin >> entities; cin.ignore();
         
-        int myBombRoundsLeft = -1;
+        Entities entitiesList(myId, entities);
         
-        
-        Entity *me = NULL;
-        std::vector<Entity> entitiesList(entities);
         for (int i = 0; i < entities; i++) {
             Entity e;
             cin >> e.entityType
@@ -243,25 +265,25 @@ int main()
                 >> e.y
                 >> e.param1
                 >> e.param2;
-            entitiesList.push_back(e);
-            if (ENTITY_TYPE_PLAYER == e.entityType && rules.myId == e.owner)
-            {
-                me = &(entitiesList.back());
-            }
-            
+            entitiesList.AddEntity(e);
             cin.ignore();
+
+
+
             //if (ENTITY_TYPE_BOMB == entityType && 0 == owner && )
             //{
             //    
             //}
         }
         
+        GridCostEstimator gridCost(grid, entitiesList);
+        cerr << gridCost;
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
         //vec.back().c
         
-        if (grid.IsBoxAround(me->x, me->y))
+        if (grid.IsBoxAround(entitiesList.Me().x, entitiesList.Me().y))
         {
             cout << "BOMB " << gridCost.maxCell.x << " " <<  gridCost.maxCell.y << endl;
         }
